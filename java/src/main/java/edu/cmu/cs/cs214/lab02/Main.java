2e8bce6cd1d28054a55e050ea93de6c26546b5cc
B232270091 <ganorgil48@gmail.com>
2025-04-25 20:09:17 +0800

    fdsfsd

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -13,6 +13,6 @@ public class Main {
         Renderer squareRenderer = new Renderer(square);
         squareRenderer.draw();
         System.out.println("Testing");
-        // ahahshaha
+        
     }
 }
